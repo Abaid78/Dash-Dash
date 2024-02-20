a7afca9789d5705ff073b3cd1690f2254d0f7b23
Abaid Ullah Aleem <103155286+Abaid78@users.noreply.github.com>
2024-02-20 10:46:51 -0800

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float jumpForce = 10f;
+    public Rigidbody2D rb;
+    public bool isGrounded = false;
+    public int extraJumps = 1;
+    public int maxExtraJumps = 1;
+    private void Start()
+    {
+       
+    }
+    void Update()
+    {
+        if (isGrounded || extraJumps > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                isGrounded = false;
+                extraJumps--;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            extraJumps = maxExtraJumps;
+        }
+    }
+
+}
